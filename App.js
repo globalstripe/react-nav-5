a9c9fe775838f5b288006341fa0c5b55c1bbd2fd
Chris Clark <chris.clark@globalstripe.com>
2021-01-01 16:18:44 +0000

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,26 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
   return (
+    <NavigationContainer>
     <View style={styles.container}>
+    <Text>Hello there!</Text>  
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
+    color: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
